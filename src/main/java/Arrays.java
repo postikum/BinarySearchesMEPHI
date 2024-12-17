217f0d94e72a5c3134e64ee856d3ab2b0a0184da
Maksim Gorbunov <postikum@gmail.com>
2024-12-17 06:19:19 +0300

    FirstCommit

---
+++ src/main/java/Arrays.java
@@ -0,0 +1,211 @@
+import java.util.Comparator;
+
+public class Arrays {
+    // 1. static int binarySearch(byte[] a, byte key)
+    public static int binarySearch(byte[] a, byte key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    // 2. static int binarySearch(byte[] a, int fromIndex, int toIndex, byte key)
+    public static int binarySearch(byte[] a, int fromIndex, int toIndex, byte key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            byte midVal = a[mid];
+
+            if (midVal < key)
+                low = mid + 1;
+            else if (midVal > key)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        return -(low + 1);
+    }
+
+    // 3. static int binarySearch(char[] a, char key)
+    public static int binarySearch(char[] a, char key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    // 4. static int binarySearch(char[] a, int fromIndex, int toIndex, char key)
+    public static int binarySearch(char[] a, int fromIndex, int toIndex, char key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            char midVal = a[mid];
+
+            if (midVal < key)
+                low = mid + 1;
+            else if (midVal > key)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        return -(low + 1);
+    }
+
+    // 5. static int binarySearch(double[] a, double key)
+    public static int binarySearch(double[] a, double key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    // 6. static int binarySearch(double[] a, int fromIndex, int toIndex, double key)
+    public static int binarySearch(double[] a, int fromIndex, int toIndex, double key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            double midVal = a[mid];
+
+            if (midVal < key)
+                low = mid + 1;  // Neither val is NaN, thisVal is smaller
+            else if (midVal > key)
+                high = mid - 1; // Neither val is NaN, thisVal is larger
+            else {
+                long midBits = Double.doubleToLongBits(midVal);
+                long keyBits = Double.doubleToLongBits(key);
+                if (midBits == keyBits)     // Values are equal
+                    return mid;             // Key found
+                else if (midBits < keyBits) // (-0.0, 0.0) or (!NaN, NaN)
+                    low = mid + 1;
+                else                        // (0.0, -0.0) or (NaN, !NaN)
+                    high = mid - 1;
+            }
+        }
+        return -(low + 1);  // key not found.
+    }
+
+    // 7. static int binarySearch(float[] a, float key)
+    public static int binarySearch(float[] a, float key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    // 8. static int binarySearch(float[] a, int fromIndex, int toIndex, float key)
+    public static int binarySearch(float[] a, int fromIndex, int toIndex, float key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            float midVal = a[mid];
+
+            if (midVal < key)
+                low = mid + 1;
+            else if (midVal > key)
+                high = mid - 1;
+            else {
+                int midBits = Float.floatToIntBits(midVal);
+                int keyBits = Float.floatToIntBits(key);
+                if (midBits == keyBits)
+                    return mid;
+                else if (midBits < keyBits)
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+        return -(low + 1);
+    }
+
+    // 9. static int binarySearch(int[] a, int key)
+    public static int binarySearch(int[] a, int key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    // 10. static int binarySearch(int[] a, int fromIndex, int toIndex, int key)
+    public static int binarySearch(int[] a, int fromIndex, int toIndex, int key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int midVal = a[mid];
+
+            if (midVal < key)
+                low = mid + 1;
+            else if (midVal > key)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        return -(low + 1);
+    }
+
+    // 11. static int binarySearch(long[] a, long key)
+    public static int binarySearch(long[] a, long key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    // 12. static int binarySearch(long[] a, int fromIndex, int toIndex, long key)
+    public static int binarySearch(long[] a, int fromIndex, int toIndex, long key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            long midVal = a[mid];
+
+            if (midVal < key)
+                low = mid + 1;
+            else if (midVal > key)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        return -(low + 1);
+    }
+
+    // 13. static int binarySearch(short[] a, short key)
+    public static int binarySearch(short[] a, short key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    // 14. static int binarySearch(short[] a, int fromIndex, int toIndex, short key)
+    public static int binarySearch(short[] a, int fromIndex, int toIndex, short key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            short midVal = a[mid];
+
+            if (midVal < key)
+                low = mid + 1;
+            else if (midVal > key)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        return -(low + 1);
+    }
+
+    // 15. static int binarySearch(T[] a, T key, Comparator c)
+    public static <T> int binarySearch(T[] a, T key, Comparator<? super T> c) {
+        return binarySearch(a, 0, a.length, key, c);
+    }
+
+    // 16. static int binarySearch(T[] a, int fromIndex, int toIndex, T key, Comparator c)
+    public static <T> int binarySearch(T[] a, int fromIndex, int toIndex, T key, Comparator<? super T> c) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int cmp = c.compare(a[mid], key);
+
+            if (cmp < 0)
+                low = mid + 1;
+            else if (cmp > 0)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        return -(low + 1);
+    }
+}
---
+++ src/main/java/Collections.java
@@ -0,0 +1,46 @@
+import java.util.Comparator;
+import java.util.List;
+
+public class Collections {
+    // 1. static int binarySearch(List> list, T key)
+    public static <T extends Comparable<? super T>> int binarySearch(List<T> list, T key) {
+        int low = 0;
+        int high = list.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            T midVal = list.get(mid);
+            int cmp = midVal.compareTo(key);
+
+            if (cmp < 0) {
+                low = mid + 1;
+            } else if (cmp > 0) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+
+    // 2. static int binarySearch(List list, T key, Comparator c)
+    public static <T> int binarySearch(List<T> list, T key, Comparator<? super T> c) {
+        int low = 0;
+        int high = list.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            T midVal = list.get(mid);
+            int cmp = c.compare(midVal, key);
+
+            if (cmp < 0) {
+                low = mid + 1;
+            } else if (cmp > 0) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+}
